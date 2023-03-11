c5cbc76148e88008cb769643512a81d7c87c2643
Madalina <pavelmadalina22@gmail.com>
2023-03-12 00:52:22 +0200

    interfata grafica si clasa polynomial

---
+++ PT2023_30223_Pavel_Madalina_Assignment_1/src/main/java/Form1.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Form1 {
+    public Form1()
+    {
+
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Am apasat butonul");
+            }
+        });
+    }
+    JFrame f;
+    JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton addButton;
+    private JButton substractButton;
+    private JButton multiplicateButton;
+    private JButton divideButton;
+    private JButton derivateButton;
+    private JButton integrateButton;
+    private JButton resultButton;
+    private JButton cancelButton;
+    private JTextField textField3;
+
+
+
+    private void createUIComponents() {
+
+
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculator");
+        frame.setIconImage(new ImageIcon("src/calculator.png").getImage());
+
+        frame.setContentPane(new Form1().panel1);
+        frame.setBounds(0,0,900,700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setIconImage(img);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- PT2023_30223_Pavel_Madalina_Assignment_1/src/main/java/org/example/Main.java
+++ PT2023_30223_Pavel_Madalina_Assignment_1/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Polynomial p1=new Polynomial();
+        p1.adaugare(5,4);
+        p1.adaugare(2,7);
+        p1.adaugare(3,8);
+        p1.afisare();
     }
 }
